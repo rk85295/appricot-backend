54ccd77e807f631cad05bcaa134d83d3cfc99b59
rk85295 <renakalil1234@outlook.com>
2025-05-08 21:32:29 -0700

    Update start-auth.js

--- api/start-auth.js
+++ api/start-auth.js
@@ -1,1 +1,13 @@
+const clientId = 'f0d4169b15e02a0e09519731ab6d6d20';
+
+export default function handler(req, res) {
+  const shop = 'appricot-dev-store2.myshopify.com';
+  const scopes = 'read_products,read_orders';
+  const redirectUri = 'https://appricot-backend-8df3.vercel.app/api/auth-callback';
+  const mobileRedirect = req.query.redirect;
+
+  const authUrl = `https://${shop}/admin/oauth/authorize?client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${encodeURIComponent(mobileRedirect)}`;
+
+  return res.redirect(authUrl);
+}
 
