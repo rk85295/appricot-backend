bbbdc74fa08ed80a090fc8b9d4574f925a04b3bf
rk85295 <renakalil1234@outlook.com>
2025-05-08 21:23:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ app.get('/start-auth', (req, res) => {
   const shop = 'appricot-dev-store2.myshopify.com';
   const redirectUri = 'https://appricot-backend-8df3.vercel.app/auth/callback';
   const scopes = 'read_products,read_orders';
-  const mobileRedirect = req.query.redirect; // <- comes from app
+  const mobileRedirect = req.query.redirect; // <- Deep link to send back to app
 
   const shopifyAuthUrl = `https://${shop}/admin/oauth/authorize?client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${encodeURIComponent(mobileRedirect)}`;
 
   console.log('🔗 Redirecting to Shopify:', shopifyAuthUrl);
   res.redirect(shopifyAuthUrl);
 });
+
 
 
 // Step 2: Shopify redirects here with ?code=...&shop=...
